47ae5016786d48492ff298a00d6418d78cc95556
Brett Bonnet <brettbonnet4@gmail.com>
2019-06-03 13:32:12 -0400

    Finished bubble_sort_by

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -3,10 +3,19 @@
 def bubble_sort_by(array)
     array.each do |num|
      0.upto(array.size-2) do |i|
-        if array[i] > array[i+1]
+        if yield(array[i], array[i+1]) > 0
           array[i], array[i+1] = array[i+1], array[i]   
         end
       end
     end
     puts array
   end 
+
+ sorted_by = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+    left.length - right.length
+  end
+
+
+  puts sorted_by
+
+  
