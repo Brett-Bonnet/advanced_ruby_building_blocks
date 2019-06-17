aecf046e73814135b62f20f9a9fc3dce3dfd9ce5
Brett Bonnet <brettbonnet4@gmail.com>
2019-06-17 11:32:45 -0400

    created my_select method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -26,7 +26,29 @@ module Enumerable
   end
   
   def my_select
-  
+    type = self.class
+    array_result = []
+    hash_result = {}
+    if type == Array
+      for i in 0..self.length-1
+        if yield(self[i])
+          array_result << self[i]
+        else
+          next
+        end
+      end
+    puts array_result
+    elsif type == Hash
+      for key, value in self
+        if yield(key, value)
+          hash_result[key] = value
+        else
+          next
+        end
+      end
+      puts hash_result
+    end
+
   end
   
   def my_all?
@@ -54,3 +76,13 @@ module Enumerable
   end
 
 end
+
+
+array_test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+hash_test = {"a" => 1, "b" => 2, "c" => 3}
+
+array_test.my_select {|x| x%2 == 0}
+hash_test.my_select {|x,y| y > 1}
+
+
+
